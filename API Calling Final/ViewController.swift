9bae7ac62ab157b794e7f7e8bb2c94e267612bc5
Jet Conway <jconway@bsd220.org>
2020-05-11 21:28:17 -0500

    Initial Commit

---
+++ API Calling Final/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  API Calling Final
+//
+//  Created by Jet Conway on 5/11/20.
+//  Copyright © 2020 Jet Conway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ API Calling FinalTests/API_Calling_FinalTests.swift
@@ -0,0 +1,34 @@
+//
+//  API_Calling_FinalTests.swift
+//  API Calling FinalTests
+//
+//  Created by Jet Conway on 5/11/20.
+//  Copyright © 2020 Jet Conway. All rights reserved.
+//
+
+import XCTest
+@testable import API_Calling_Final
+
+class API_Calling_FinalTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
